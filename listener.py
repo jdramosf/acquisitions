0edde71934cfead61622048f302b2024f3e5df02
jdramosf <juan.fuentes@bazaarvoice.com>
2018-02-26 21:04:29 -0600

    Pulling raw-data and doing some pre-processing

--- listener.py
+++ listener.py
@@ -2,20 +2,26 @@
 import os
 import json
 import datetime
-import tweepy
 import pprint
-from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
-
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from datetime import datetime
+from dateutil.parser import parse
+import tweepy
 import settings
+import pandas
+
+nltk.download('stopwords')
+nltk.download('punkt')
+stop_words = set(stopwords.words('english'))
 
 ckey = settings.CONSUMER_KEY
 csecret = settings.CONSUMER_SECRET
 atoken = settings.TOKEN_ACCESS
 asecret = settings.TOKEN_SECRET
 
-class Listener(StreamListener):
+class Listener(tweepy.StreamListener):
 
 	def on_data(self, data):
 		parsed = json.loads(data)
@@ -25,14 +31,23 @@ class Listener(StreamListener):
 	def on_error(self, status):
 		print(status, "here")
 
-def get_tweets_by_date(user, api):
+def get_tweets_by_date(user, limit, api):
+	result = []
+	limit = datetime.strptime(limit, '%m-%d-%Y')
 	for status in tweepy.Cursor(api.user_timeline,
 								id = user).items():
-		pprint.pprint(status._json)
-		#print(json.dumps(parsed, indent = 2))
+		if status.created_at < limit:
+			row = {}
+			word_tokenized = word_tokenize(status.text)
+			row['user'] = user
+			row['created_at'] = status.created_at
+			row['text'] = ' '.join([w for w in word_tokenized if not w in stop_words])
+			result.append(row)
+	
+	return result
 
 if __name__ == '__main__':
-	auth = OAuthHandler(ckey, csecret)
+	auth = tweepy.OAuthHandler(ckey, csecret)
 	auth.set_access_token(atoken, asecret)
 
 	api = tweepy.API(auth)
@@ -44,10 +59,8 @@ if __name__ == '__main__':
 	acquirees = ["the60dB", "Dialogflow", "itasoftware", "Zyncrender", "Anvato"]
 	date_acquired = ["10-11-2017", "9-19-2016", "4-12-2011", "8-26-2014", "7-8-2016"]
 
-	for acquiree in acquirees:
-		get_tweets_by_date(acquiree, api)
-
-
-
-
+	for i in range(len(acquirees)):
+		result = get_tweets_by_date(acquirees[i], date_acquired[i], api)
+		df = pandas.DataFrame(result)
+		df.to_csv('raw_data/' + acquirees[i] + '.csv', encoding = 'utf-8')
 
